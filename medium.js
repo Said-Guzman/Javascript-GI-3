2386b64ea0062991ca516fc1228e86b1512915d9
Said-Guzman <112099452+Said-Guzman@users.noreply.github.com>
2022-10-02 21:07:04 -0400

    more javascript.......

--- medium.js
+++ medium.js
@@ -16,26 +16,31 @@
 
 var sharePizza = cutPizzaSlices(8)
 
+var div = 2
 
 function cutPizzaSlices(x){
     return function(){
-        return x 
+        return "Each person get's " + x / div + " slices of Pizza"
     }
 }
+
 
 
 
 console.log(sharePizza(2));
 //   // prints "Each person gets 4.00 slices of pizza"
 
+ var div = 3
 
-
+function cutPizzaSlices(x){
+    return function(){
+        return "Each person get's " + x / div + " slices of Pizza"
+    }
+}
 
 
 console.log(sharePizza(3)); 
 //   // prints "Each person gets 2.67 slices of pizza"
-
-
 
 
 
